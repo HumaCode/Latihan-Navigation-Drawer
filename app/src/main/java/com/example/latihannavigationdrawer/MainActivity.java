e39beb38562874a58c1a5e1fc2b7dfe294e96ddf
humaidi <humaidizakaria1998@gmail.com>
2022-06-08 21:36:46 +0700

    Latihan Navigation Drawer

---
+++ app/src/main/java/com/example/latihannavigationdrawer/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.latihannavigationdrawer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    NavigationView navigationView;
+
+    Fragment fragment;
+    FragmentTransaction transaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        navigationView = findViewById(R.id.navigation_view);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                openFragment(item.getItemId());
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawers();
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    private void openFragment(int itemID) {
+        fragment = null;
+
+        switch (itemID){
+            case R.id.nav_dashboard:
+                fragment = new FragmentDashboard();
+                break;
+            case R.id.nav_account:
+                fragment = new FragmentAccount();
+                break;
+            case R.id.nav_notification:
+                fragment = new FragmentNotification();
+                break;
+            case R.id.nav_setting:
+                fragment = new FragmentSetting();
+                break;
+            case R.id.nav_help:
+                fragment = new FragmentHelp();
+                break;
+        }
+
+        if(fragment != null){
+            transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_layout, fragment);
+            transaction.commit();
+        }
+
+        drawerLayout.closeDrawers();
+    }
+}
